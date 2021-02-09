457464bfb95f1abd68e2b18cee76599272773f7f
Montse <montze94@gmail.com>
2021-02-09 00:05:41 -0600

    [UPDATE] PPadding to Page component

--- src/components/Page/Page.tsx
+++ src/components/Page/Page.tsx
@@ -1,5 +1,6 @@
 import React, { ReactNode, ReactElement } from 'react';
 
+import { PageContainer } from './Page.styles';
 import { Helmet } from 'react-helmet';
 
 type PageProps = {
@@ -12,7 +13,7 @@ const Page = ({ title, children }: PageProps): ReactElement => (
         <Helmet>
             <title>{title || null}</title>
         </Helmet>
-        {children}
+        <PageContainer>{children}</PageContainer>
     </>
 );
 
